9797c8bfaef87e4697a9fc0fb41f0332e5d602b6
git <timo@1amail.net>
2019-01-26 18:41:38 +0100

    Aktuelle Version

--- zutaten-manager/Zutatenmanager.php
+++ zutaten-manager/Zutatenmanager.php
@@ -1,55 +1,14 @@
 <?php
 
 class Zutatenmanager{
-    
-    function Zutatenmanager(){
-        $this->managerHeadline();
-        //Initial alle Zutaten aufrufen
-        $this->listWarengruppen();
-        echo("<hr />");
-        $this->listZutaten();
-        echo ("<hr />");
-        $this->listEinheiten();
-        echo ("<hr />");
-        $this->listRezept(1);
-        echo ("<hr />");
-        $this->printZutatenString(1);
+    //Erstmal nur statische Methodenb
+    function __construct(){
     }
     
-    function loadWarengruppen(){
+    static function loadWarengruppen(){
         global $wpdb; //Wp Datenbank-Objekt
-        $wg = $wpdb->get_results("SELECT * FROM ZM_Warengruppe");
+        $wg = $wpdb->get_results("SELECT * FROM ".$wpdb->prefix."ZM_Warengruppe");
         return $wg;
-    }
-    
-    function managerHeadline(){
-        //echo('<div class="postbox"><a href="#">Zutat hinzufügen</a> | </div>');
-    }
-    
-    //Listet alle Zutaten auf
-    function listZutaten(){
-        $wg = $this->loadZutaten($pkWg);
-        echo "<b>Zutaten</b>";
-        echo("<ul>");
-        
-            foreach($wg as $warengruppe){
-                echo('<li>(' . $warengruppe->FK_Warengruppe . ') <input type="text" name="textWarengruppe" value="' . $warengruppe->Zutat_Text . '" /> <a href="#">Löschen</a></li>');
-            }
-        echo('<li>(n) <input type="text" name="addWarengruppe" value="" /> <a href="#">Hinzufügen</a></li>');
-        echo("</ul>");
-    }
-    
-    function listWarengruppen(){
-        $wg = $this->loadWarengruppen();
-        echo "<b>Warengruppen</b>";
-        echo("<ul>");
-        
-            foreach($wg as $warengruppe){
-                echo('<li>(' . $warengruppe->PK_Warengruppe . ') <input type="text" name="textZutat" value="' . $warengruppe->Bezeichnung . '" /> <a href="#">Löschen</a></li>');
-            }
-        echo('<li>(n) <input type="text" name="addZutat" value="" /> <a href="#">Hinzufügen</a></li>');
-        echo("</ul>");
-        
     }
     
     //Wird keine Warengruppe übergeben, werden alle Zutaten mit ihren entsprechenden Warengruppen zurückgegeben.
@@ -59,56 +18,42 @@ class Zutatenmanager{
         if($pkWarengruppe !== null){
             $pkCond = " AND PK_Zutat = " . $pkWarengruppe;
         }
-        $zt = $wpdb->get_results("SELECT PK_Zutat, FK_Warengruppe, PK_Warengruppe, zt.Bezeichnung AS 'Zutat_Text', wg.Bezeichnung AS 'Warengruppe_Text' 
-                                  FROM ZM_Zutat as zt, ZM_Warengruppe as wg WHERE zt.FK_Warengruppe = wg.PK_Warengruppe  ". $pkCond ."  
+        $zt = $wpdb->get_results("SELECT PK_Zutat, FK_Warengruppe, PK_Warengruppe, zt.Bezeichnung AS 'Zutat_Text', wg.Bezeichnung AS 'Warengruppe_Text'
+                                  FROM ".$wpdb->prefix."ZM_Zutat as zt, ".$wpdb->prefix."ZM_Warengruppe as wg WHERE zt.FK_Warengruppe = wg.PK_Warengruppe  ". $pkCond ."
                                   ORDER BY zt.FK_Warengruppe, zt.PK_Zutat, zt.Bezeichnung;");
         return $zt;
-    }
-    
-    function listRezept($id){
-        global $wpdb;
-        $rslt = $wpdb->get_results("SELECT * FROM ZM_Rezept_Map as m, ZM_Zutat as z, ZM_Einheit as e
-                                    WHERE e.PK_Einheit = m.FK_Einheit AND
-                                    z.PK_Zutat = m.FK_Zutat AND m.FK_WP_Posts_ID = ". $id);
-        
-        echo('<b>Am Post mit der ID ' . $id . ' hängen folgende Zutaten:</b> ');
-        echo('<ul>');
-            foreach ($rslt as $zutat){
-                echo('<li>-' . $zutat->Menge . ' ' . $zutat->Label . ' ' . $zutat->Bezeichnung .  '(' . $zutat->Zusatz . ') </li>');
-            }
-        echo('</ul>');
-    }
-    
-    
-    function listEinheiten(){
-        global $wpdb;
-        //$myposts = get_posts('');
-        $rslt = $wpdb->get_results("SELECT * FROM ZM_Einheit");
-        echo ("In der Datenbank vorhanden Einheiten:");
-        echo('<ul>');
-        
-            foreach($rslt as $einheit){
-                echo('<li>' . $einheit->Typ. ' - ' . $einheit->Label . '</li>');
-            }
-        
-        echo('</ul>');
     }
     
     function deleteZutat($id){
         
     }
     
-    function addZutat($name, $usw){
-        
+    
+    function deleteIngredient($id){//funktioniert nicht
+        global $wpdb;
+        $data = "Select from $wpdb WHERE id = $id";
+        $sql = "DELETE FROM $wpdb WHERE name = $data";
     }
+    
+    static function addZutat($name, $fk_warengruppe){
+        global $wpdb;
+        $table = $wpdb->prefix.'ZM_Zutat';
+        $data = array('FK_Warengruppe' => $fk_warengruppe, 'Bezeichnung' => $name);
+        //$format = array('%d','%s');
+        $wpdb->insert($table,$data,$format);
+        return $my_id = $wpdb->insert_id;
+    }
+    
     function printZutatenString(){
-       echo Zutatenmanager::buildRezeptString(1, true);
+        echo Zutatenmanager::buildRezeptString(754, true);
     }
-   public static function buildRezeptString($postID =-1, $htmlMode=false){
+    
+    //Baut den RezepteString
+    public static function buildRezeptString($postID =-1, $htmlMode=false){
         global $wpdb;
         if ($postID == -1) return "";
         
-        $arrRezept = $wpdb->get_results("SELECT * FROM ZM_Einheit AS zme, ZM_Rezept_Map AS zmrm, ZM_Zutat AS zmz WHERE
+        $arrRezept = $wpdb->get_results("SELECT * FROM ".$wpdb->prefix."ZM_Einheit AS zme, ".$wpdb->prefix."ZM_Rezept_Map AS zmrm, ".$wpdb->prefix."ZM_Zutat AS zmz WHERE
 									zme.PK_Einheit = zmrm.FK_Einheit AND
 									zmz.PK_Zutat = zmrm.FK_Zutat AND
 									zmrm.FK_WP_Posts_ID = ".$postID."
@@ -123,52 +68,17 @@ class Zutatenmanager{
             if($gruppeNMinus1 !== $zutat->Gruppe && !$zutat->Gruppe == ""){ //TODO: Null-Verhalten != "" und Sortierung checken
                 $obLine .= "#" . $zutat->Gruppe . "#" . ($htmlMode ? "<br/>" :"\r\n"); //Ändert sich die Gruppe, schreibe diese zusätzlich als eigene Zeile einmalig davor
             }
-
-            $obLine .= $zutat->Menge . " " . $zutat->Label . " " . $zutat->Bezeichnung . ($zutat->Zusatz == "" ? " " : " (". $zutat->Zusatz .")") . ($htmlMode ? "<br/>" :"\r\n");
-           // print_r($zutat);
+            
+            $obLine .= round($zutat->Menge, 2) . " " . $zutat->Label . " " . $zutat->Bezeichnung . ($zutat->Zusatz == "" ? " " : " (". $zutat->Zusatz .")") . ($htmlMode ? "<br/>" :"\r\n");
+            // print_r($zutat);
             $obString .= $obLine;
             
             $gruppeNMinus1 = $zutat->Gruppe;
             
             
-        }		
-            return $obString;	
+        }
+        return $obString;
     }
     
 }
-
-
-
-// class Einheit{
-//     //const einheiten = array("mg", "g", "kg", "ml", "l");
-    
-//     var $label = "";
-//     var $typ = "";
-    
-//     public function Einheit($id){
-        
-//     }
-    
-//     public static function getAllEinheiten(){
-//         global $wpdb;
-//         $einheiten = array();
-//         $rsltEinheiten = $wpdb->get_results("SELECT * FROM ZM_Einheit");
-//         print_r($rsltEinheiten);
-//         $i=0;
-//         foreach($rsltEinheiten as $einheit){
-//             $tmpEinheit = new Einheit();
-//             $tmpEinheit->typ = $einheit["Typ"];
-//             $tmpEinheit->label = $einheit['Label'];
-            
-//             if($tmpEinheit->typ == Einheit::einheiten['mg']){
-//                 print_r($tmpEinheit);
-//             }
-            
-//             echo $einheit['Label'];
-//             $i++;
-//         }
-       
-//     }
-// }
-
 
--- zutaten-manager/zutaten.php
+++ zutaten-manager/zutaten.php
@@ -1,60 +1,79 @@
 <?php
 /*
-Plugin Name: Zutatenverwaltung
-Plugin URI: https://herdzeit.de/
-Description: Ein Plugin zur Zutatenverwaltung mit eigener Datenhaltung.
-Version: 0.0.1
-Author: Herdzeit
-Author URI: Herdzeit.de
-License: 
-Text Domain: zutaten
-*/
+ Plugin Name: Zutatenverwaltung
+ Plugin URI: https://herdzeit.de/
+ Description: Ein Plugin zur Zutatenverwaltung mit eigener Datenhaltung.
+ Version: 0.0.1
+ Author: Herdzeit
+ Author URI: Herdzeit.de
+ License:
+ Text Domain: zutaten
+ */
 require_once 'Zutatenmanager.php';
 
-//Diese Aktionen registrieren "hooks/fiter" auf die Funktionen die im 2. Parameter stehen
-//add_filter('the_content', 'hello_world'); //Content eines Beitrags manipulieren
-//add_action('wp_head', 'add_to_head'); //Fügt Inhalt dem Headerbereich auf der Seite (nicht Admin) hinzu
-add_action('admin_menu', 'zutaten_management');
+function pw_load_scripts($hook) {
+    echo ("<span style=\"color:red\">fuck you</span>");
+    if( $hook != 'toplevel_page_zutaten' ) return;
+    
+    wp_enqueue_script( 'zm-dataTables', plugins_url( 'zutaten/js/zm-dt.js' , dirname(__FILE__) ) );
+}
+add_action('admin_enqueue_scripts', 'pw_load_scripts');
 
-//Wird für jeden Beitrag, bzw. dessen Inhalt ausgeführt.
-// function hello_world($content) {
-//     return strtoupper($content); //Macht den gesamten Text eines Posts to upper
-// }
+function admin_style($hook) {
+    //     if( $hook != 'toplevel_page_zutaten' ) return
+    
+    wp_enqueue_style('admin-styles', plugins_url( 'zutaten/css/dt.css' , dirname(__FILE__) ) );
+}
+add_action('admin_enqueue_scripts', 'admin_style');
 
-//Fügt Inhalt dem Headerbereich auf der Seite (nicht Admin) hinzu
-// function add_to_head(){
-//     echo ('<script type="text/javascript">alert("tito");</script>');
-// }
 //Fügt im Admin Menü den Punkt Zutaten-Manager hinzu und ruft die Funktion zutatenManagerInit auf, bei einem Klick auf diesen
 function zutaten_management(){
-    add_menu_page('Zutatenmanagement', 'Zutaten-Manager', 'manage_options', 'zutaten', 'zutatenManagerInit');    
+    add_menu_page('Zutatenmanagement', 'Zutaten-Manager', 'manage_options', 'zutaten', 'zutatenManagerInit');
 }
- 
+//Diese Aktionen registrieren "hooks/fiter" auf die Funktionen die im 2. Parameter stehen
+//add_filter('the_content', 'hello_world'); //Content eines Beitrags manipulieren
+//add_action('admin_head', 'add_to_head'); //Fügt Inhalt dem Headerbereich auf der Seite (nicht Admin) hinzu
+add_action('admin_menu', 'zutaten_management');
+
+function zmAjaxHandler() {
+    global $wpdb; // this is how you get access to the database
+    
+    $whatever = intval( $_POST['whatever'] );
+    switch($_POST['function']){
+        
+        case "loadProductGroups":
+            echo ajaxLoadProductGroups();
+            break;
+        case "zmAllIngredients":
+            echo ajaxLoadAllIngredients();
+            break;
+        case "addIngredient":
+            echo ajaxAddIngredient();
+            break;
+    }
+    
+    wp_die(); // this is required to terminate immediately and return a proper response
+}
+add_action( 'wp_ajax_zmAJAX', 'zmAjaxHandler' );
+
 //Die Funktion, die inital im Backend aufgerufen wird, wenn man den Punkt "Zutaten-Manager" auswählt
 function zutatenManagerInit(){
-//     global $wpdb;
-//     listWarengruppen();
-    new Zutatenmanager(); //-> siehe Zutatenmanager.php
+    include 'zutaten_Overview.php';
 }
 
-// function listWarengruppen(){
-//     foreach(loadWarengruppen() as $warengruppe){
-//         echo $warengruppe;
-//     }
-//     echo("<ul>");
-    
-//     echo('<li>Zutat 1 <a href="#">Löschen</a></li>');
-    
-//     echo("</ul>");
-// }
 
-// function loadWarengruppen(){
-//     global $wpdb;
-//     $wg = $wpdb->get_results("SELECT * FROM ZM_Warengruppe");
-//     print_r($wg);
-//     return $wg;
-// }
-
+//    AJAX Kram erstmal hier..
+function ajaxLoadProductGroups(){return json_encode(Zutatenmanager::loadWarengruppen());}
+function ajaxLoadAllIngredients(){
+    global $wpdb;
+    return '{"data":'.json_encode($wpdb->get_results("SELECT PK_Zutat, FK_Warengruppe, Bezeichnung, Energie_KJ, Fett,
+  Fett_gesaettigt, Kohlenhydrate, Kohlenhydrate_Zucker, Eiweiss, Salz, Einheit, immer_zuhause, created,
+  (SELECT COUNT(FK_Zutat) FROM ".$wpdb->prefix."ZM_Rezept_Map WHERE FK_Zutat = PK_Zutat) as 'referenzen' FROM ".$wpdb->prefix."ZM_Zutat")).'}';
+}
+function ajaxAddIngredient(){
+    return Zutatenmanager::addZutat($_POST['bezeichnung'],$_POST['FK_Warengruppe']);
+}
+///   //AJAX
 
 
 ?>
